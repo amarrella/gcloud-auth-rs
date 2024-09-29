f5328cb0a714c7aeac2d7be613b0056f85beac67
Alessandro Marrella <alessandro@bandstand.dev>
2024-09-29 16:18:18 +0200

    clap command subcommand

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Auth
+    Auth {
+        #[command(subcommand)]
+        auth_commands: Option<AuthCommands>,
+    },
+}
+
+#[derive(Subcommand)]
+enum AuthCommands {
+    PrintIdToken,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
 }
